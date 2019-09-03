da97ad97fd91f253129bd179fe5872ac02ba6f97
Josh Al-DIn <joshaldin@gmail.com>
2019-09-03 12:48:10 +1000

    added dynamic routing and contact form logic

--- run.py
+++ run.py
@@ -1,14 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 import os
 import json
 
 
 app = Flask(__name__)
-
+app.secret_key = 'some_secret'
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/about')
 def about():
@@ -17,16 +18,32 @@ def about():
         data = json.load(json_data)
     return render_template('about.html', page_title='About', company=data)
 
-@app.route('/contact')
+
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+
+    with open('data/company.json', 'r') as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj['url'] == member_name:
+                member = obj
+    return render_template('member.html', member=member)
+
+
+@app.route('/contact', methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        flash(f"Thanks {request.form['name']}, we have received your message!")
     return render_template('contact.html', page_title='Contact')
-    
+
+
 @app.route('/careers')
 def careers():
     return render_template('careers.html', page_title='Careers')
 
 
 if __name__ == "__main__":
-    app.run(host=os.environ.get('IP'), 
-        port=int(os.environ.get('PORT')),
-        debug=True)
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
